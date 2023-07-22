c9b583883ca74584321d286b5089e2f2e508a563
Avi6584 <89294923+Avi6584@users.noreply.github.com>
2023-07-22 16:26:16 +0530

    Update 04. Data Analysis.sql

--- 04. Data Analysis.sql
+++ 04. Data Analysis.sql
@@ -1,27 +1,27 @@
--- Data Analysis
+-- Data Analysis Stage
 
--- bikes types used by riders
+-- bike types used by riders
 
 SELECT member_casual, rideable_type, COUNT(*) AS total_trips
 FROM `2022_tripdata.cleaned_combined_data`
 GROUP BY member_casual, rideable_type
 ORDER BY member_casual, total_trips;
 
--- no. of trips per month
+-- no.of trips per month
 
 SELECT month, member_casual, COUNT(ride_id) AS total_trips
 FROM `2022_tripdata.cleaned_combined_data`
 GROUP BY month, member_casual
 ORDER BY member_casual;
 
--- no. of trips per day of week
+-- no.of trips per day of week
 
 SELECT day_of_week, member_casual, COUNT(ride_id) AS total_trips
 FROM `2022_tripdata.cleaned_combined_data`
 GROUP BY day_of_week, member_casual
 ORDER BY member_casual;
 
--- no. of trips per hour
+-- no.of trips per hour
 
 SELECT EXTRACT(HOUR FROM started_at) AS hour_of_day, member_casual, COUNT(ride_id) AS total_trips
 FROM `2022_tripdata.cleaned_combined_data`
